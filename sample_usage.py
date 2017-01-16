005023fbba3abacaef4a10a4f008745aa24a6b1b
Philipp Grabovoy <gra.filipp@gmail.com>
2017-01-16 10:56:44 +0300

    unstable, combined client and server and mitm

--- sample_usage.py
+++ sample_usage.py
@@ -2,6 +2,7 @@
 
 
 import socket
+import time
 from contextlib import closing
 from tcp_mitm import TcpMitm
 from threading import Thread
@@ -19,35 +20,52 @@ def server_routine(port):
                 continue
             msg = data.decode("utf-8")
             print("Received by server: {}".format(msg))
-            break
+            if msg == "exit":
+                break
         conn.close()
 
     print("Server finished")
 
-def client_routine(server_port, msg):
+
+def client_routine(server_port, msgs):
     with socket.socket() as sock:
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         sock.connect(("127.0.0.1", server_port))
-        sock.send(msg.encode("utf-8"))
+
+        for msg in msgs:
+            sock.send(msg.encode("utf-8"))
+
     print("Client finished")
 
 
 def packet_forward(server_port, middle_port):
     with closing(TcpMitm(server_port, middle_port)) as mitm:
         receiver = mitm.recv()
+        # i = False
+
         while True:
             pkt = next(receiver)
+            # to check buffer overflowing
+            # if not i:
+            #     time.Sleep(5)
+            #     i = True
+            print("Mitm received {} bytes".format(len(pkt)))
             mitm.send(pkt)
+
+    print("Mitm finished")
 
 
 if __name__ == "__main__":
-    # server_port = 10040
-    # msg_len = 5
-    # ts = Thread(name="server", target=server_routine, args=(server_port, ))
-    # ts.start()
-    # tc = Thread(name="client", target=client_routine, args=(server_port, "a" * msg_len))
-    # tc.start()
+    server_port = 20040
+    middle_port = 10020
+    msgs = ["aaaaaaa", "aaaa", "exit"]
+    ts = Thread(name="server", target=server_routine, args=(server_port, ))
+    ts.start()
+    tc = Thread(name="client", target=client_routine, args=(middle_port, msgs))
+    tc.start()
 
-    packet_forward(20040, 10020)
+    # tm = Thread(name="mitm", target=packet_forward, args=(server_port, middle_port))
+    packet_forward(server_port, middle_port)
 
-    # tc.join()
-    # ts.join()
+    tc.join()
+    ts.join()
